add2d1187e763b24101f0c5cad1fc9fd86cc5954
FlutLab.io <>
2022-06-02 02:57:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'app/views/splash_view.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
