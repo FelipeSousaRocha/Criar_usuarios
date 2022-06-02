57005136fd855f4c42f3406a19b6d082b80d7e3d
FlutLab.io <>
2022-06-02 18:37:32 +0000

    Autosave

--- lib/app/views/user_form.dart
+++ lib/app/views/user_form.dart
@@ -32,6 +32,8 @@ class _UserFormState extends State<UserForm> {
   Widget build(BuildContext context) {
     final user = ModalRoute.of(context)?.settings.arguments as User;
 
+    print(user);
+
     _loadFormData(user);
 
     return Scaffold(
--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,6 +1,6 @@
 import 'package:fluttercrud/models/user.dart';
 
-const DUMMY_USERS = {
+final DUMMY_USERS = {
   '1': User(id: '1', name: 'Maria', password: 'maria123', email: 'maria@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'),
   '2': User(id: '2', name: 'Joao', password: 'joao123', email: 'joao@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_960_720.png'),
   '3': User(id: '3', name: 'Jose', password: 'jose123', email: 'jose@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png'),
