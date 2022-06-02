751fa4858abee1d7fb18d0cb72f70cdaae84b4ed
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-06-02 00:23:11 -0300

    ChafierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'app/views/splash_view.dart';
 // ignore: unused_import
 import 'package:fluttercrud/models/user.dart';
