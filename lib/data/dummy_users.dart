b288e3d779b1452141dff5893b0cae324b9d935b
FlutLab.io <>
2022-06-02 18:58:05 +0000

    adawdwa

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -4,4 +4,7 @@ final Map<String, User> DUMMY_USERS = {
   '1': const User(id: '1', name: 'Maria', password: 'maria123', email: 'maria@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'),
   '2': const User(id: '2', name: 'Joao', password: 'joao123', email: 'joao@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_960_720.png'),
   '3': const User(id: '3', name: 'Jose', password: 'jose123', email: 'jose@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png'),
+  '4': const User(id: '4', name: 'Felipe', password: 'Felipe123', email: 'Felipe@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635456_960_720.png'),
+  '5': const User(id: '5', name: 'Sousa', password: 'Sousa123', email: 'Sousa@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2020/06/04/12/57/emoji-5258649_960_720.png'),
+  '6': const User(id: '6', name: 'Rocha', password: 'Rocha123', email: 'Rocha@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
 };
